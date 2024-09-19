8a9235eebf2f78a6fdf8b703d12b374c3b9fbd9e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-09-19 15:05:42 +0800

    Step 2: Create a new pattern

--- section20/simon-game/game.js
+++ section20/simon-game/game.js
@@ -1,1 +1,14 @@
-console.log('Simon says...')
+const gamePattern = []
+
+const buttonColors = [
+  'red',
+  'blue',
+  'green',
+  'yellow',
+]
+
+function nextSequence() {
+  const randomNumber = Math.floor(Math.random() * 4)
+  const randomChosenColor = buttonColors[randomNumber]
+  gamePattern.push(randomChosenColor)
+}
