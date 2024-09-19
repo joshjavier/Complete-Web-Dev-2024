1d300805becd7f769ef806b0ea32cf9766c9f516
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-09-19 15:41:33 +0800

    Step 3: Button animation and sounds

--- section20/simon-game/game.js
+++ section20/simon-game/game.js
@@ -12,3 +12,24 @@ function nextSequence() {
   const randomChosenColor = buttonColors[randomNumber]
   gamePattern.push(randomChosenColor)
 }
+
+function flashButton(el) {
+  $(el).fadeOut(100).fadeIn(100)
+}
+
+function playSound(el) {
+  const audiomap = {
+    red: new Audio('sounds/red.mp3'),
+    blue: new Audio('sounds/blue.mp3'),
+    green: new Audio('sounds/green.mp3'),
+    yellow: new Audio('sounds/yellow.mp3'),
+  }
+
+  audiomap[$(el).attr('id')].play()
+}
+
+// Add click handlers to buttons
+$('.btn').on('click', function () {
+  flashButton(this)
+  playSound(this)
+})
