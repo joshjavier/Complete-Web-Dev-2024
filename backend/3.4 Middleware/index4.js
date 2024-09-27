d72f6c5bc88d008ba68e0e429b8eb519fad81e35
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-09-27 15:44:32 +0800

    Finish Band Name Generator challenge

--- backend/3.4 Middleware/index4.js
+++ backend/3.4 Middleware/index4.js
@@ -1,7 +1,22 @@
 import express from "express";
+import path from "node:path";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("tiny"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./public/index.html"));
+});
+
+app.post("/submit", (req, res) => {
+  const bandName = req.body.street + req.body.pet;
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
