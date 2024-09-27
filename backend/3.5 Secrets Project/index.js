d11ac9544cfe99eaff4da4574e4fb43f99ae259d
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-09-27 17:59:16 +0800

    Finish Secrets Project

--- backend/3.5 Secrets Project/index.js
+++ backend/3.5 Secrets Project/index.js
@@ -1,3 +1,29 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import express from "express";
+import path from "node:path";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./public/index.html"));
+});
+
+app.post("/check", (req, res) => {
+  const { password } = req.body;
+
+  if (password === "ILoveProgramming") {
+    res.sendFile(path.resolve("./public/secret.html"));
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
