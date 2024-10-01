d5eeb15988f2261082e7af16e3aad1cd0de97dd8
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-01 19:43:00 +0800

    Implement GET routes using initial dummy data

--- capstone3/index.js
+++ capstone3/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { postCollection, userCollection } from "./data.js";
 
 const app = express()
 const port = 3000
@@ -7,7 +8,17 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  const posts = [...postCollection.values()].map(post => ({
+    ...post,
+    author: userCollection.get(post.userId),
+  }))
+  res.render('index.ejs', { posts })
+})
+
+app.get('/post/:id', (req, res) => {
+  const post = postCollection.get(Number(req.params.id))
+  post.author = userCollection.get(post.userId)
+  res.render('post.ejs', { post })
 })
 
 app.listen(port, () => {
