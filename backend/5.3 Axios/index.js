39024e441e1951a75e297bc4f74681d0984a4460
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-09 07:47:25 +0800

    Finish Bored API Axios challenge

--- backend/5.3 Axios/index.js
+++ backend/5.3 Axios/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import { rng } from "./utils.js";
 
 const app = express();
 const port = 3000;
+const baseUrl = "https://bored-api.appbrewery.com";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,19 +15,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
-    const result = response.data;
-    res.render("index.ejs", { data: result });
+    const response = await axios.get(`${baseUrl}/random`);
+    res.render("index.ejs", { data: response.data });
   } catch (error) {
-    console.error("Failed to make request:", error.message);
+    let errorMessage = error.message;
+    if (error.response.status === 404) {
+      errorMessage = "No activities match your criteria.";
+    }
+
+    console.error("Failed to make request:", errorMessage);
     res.render("index.ejs", {
-      error: error.message,
+      error: errorMessage,
     });
   }
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  const { type, participants } = req.body;
+
+  let url, pickRandom = false;
+
+  if (!type && !participants) {
+    url = `${baseUrl}/random`;
+  } else {
+    const params = new URLSearchParams(req.body);
+    url = `${baseUrl}/filter?${params.toString()}`;
+    pickRandom = true;
+  }
+
+  try {
+    const response = await axios.get(url);
+
+    let { data } = response;
+    if (pickRandom) {
+      data = response.data[rng(response.data.length)];
+    }
+
+    res.render("index.ejs", { data });
+  } catch (error) {
+    let errorMessage = error.message;
+    if (error.response.status === 404) {
+      errorMessage = "No activities match your criteria.";
+    }
+
+    console.error("Failed to make request:", errorMessage);
+    res.render("index.ejs", {
+      error: errorMessage,
+    });
+  }
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
---
+++ backend/5.3 Axios/utils.js
@@ -0,0 +1,1 @@
+export const rng = (max) => Math.floor(Math.random() * max);
