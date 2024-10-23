133e9e686696a85d643b9935ddc540595b51700f
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-23 15:52:31 +0800

    Complete all challenges in the Blog API Project

--- backend/6.1 Blog API Project/index.js
+++ backend/6.1 Blog API Project/index.js
@@ -41,14 +41,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
 
 //CHALLENGE 2: GET a specific post by id
+app.get('/posts/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const post = posts.find(p => p.id === id)
+  if (post) {
+    res.send(post)
+  } else {
+    res.status(404).send({ error: 'Post not found' })
+  }
+})
 
 //CHALLENGE 3: POST a new post
+app.post('/posts', (req, res) => {
+  const id = Math.max(...posts.map(p => p.id)) + 1
+
+  const newPost = {
+    id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  }
+
+  posts.push(newPost)
+  res.send(newPost)
+})
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch('/posts/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const { title, content, author } = req.body
+  const post = posts.find(p => p.id === id)
+
+  if (post) {
+    let updatedPost = post
+    if (title) updatedPost.title = title
+    if (content) updatedPost.content = content
+    if (author) updatedPost.author = author
+    posts = posts.map(p => p.id === id ? updatedPost : p)
+    res.send(updatedPost)
+  } else {
+    res.status(404).send({ error: 'Post not found' })
+  }
+})
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete('/posts/:id', (req, res) => {
+  const id = Number(req.params.id)
+  posts = posts.filter(p => p.id !== id)
+  res.sendStatus(204)
+})
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
