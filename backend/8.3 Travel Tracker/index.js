7b4b95a8938a87e71c771a1d8f6c15382f6176cf
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-05 05:11:30 +0800

    Highlight visited countries on map

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client("postgres://postgres@localhost:5432/world");
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  const countries = result.rows.map(c => c.country_code);
+  db.end();
+
+  res.render("index.ejs", {
+    countries,
+    total: countries.length,
+  });
 });
 
 app.listen(port, () => {
