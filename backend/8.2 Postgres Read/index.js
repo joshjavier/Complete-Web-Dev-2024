9bdc0cf38cd0f535f1e07596da7c952cb1e49c3e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-05 02:25:50 +0800

    Finish Postgres Read exercise

--- backend/8.2 Postgres Read/index.js
+++ backend/8.2 Postgres Read/index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client("postgres://postgres@localhost:5432/world");
+
+db.connect();
+
+let quiz = {}
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.log("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
@@ -24,7 +39,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
