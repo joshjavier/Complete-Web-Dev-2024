0854faea870f06f2cfc83582f75951555ec16fd9
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-05 05:55:14 +0800

    Add route for adding visited countries

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -15,12 +15,25 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
   const result = await db.query("SELECT country_code FROM visited_countries");
   const countries = result.rows.map(c => c.country_code);
-  db.end();
+  // db.end();
 
   res.render("index.ejs", {
     countries,
     total: countries.length,
   });
+});
+
+app.post("/add", async (req, res) => {
+  const { country } = req.body;
+
+  // Get the corresponding country code
+  const result = await db.query(`SELECT country_code FROM countries WHERE country_name ILIKE '${country}%'`);
+  const country_code = result.rows[0]?.country_code;
+
+  // Add country code to visited_countries table
+  await db.query(`INSERT INTO visited_countries (country_code) VALUES ('${country_code}')`);
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
