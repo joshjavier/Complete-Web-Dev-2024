6cdb21da019f3fe838a3e7b7b23df1a3cbf228c6
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-05 23:12:22 +0800

    Add error messages

--- backend/8.3 Travel Tracker/index.js
+++ backend/8.3 Travel Tracker/index.js
@@ -25,23 +25,32 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   const { country } = req.body;
+  let error, countries;
 
   try {
     // Get the corresponding country code
     const result = await db.query('SELECT country_code FROM countries WHERE country_name ILIKE $1', [country]);
+    const visitedCountriesResult = await db.query("SELECT country_code FROM visited_countries");
+    countries = visitedCountriesResult.rows.map(c => c.country_code);
 
     // Add country code to visited_countries table
     if (result.rowCount) {
       const country_code = result.rows[0].country_code;
       await db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [country_code]);
+    } else {
+      throw new Error('Country does not exist, try again');
     }
   } catch (error) {
     if (error.message.includes('duplicate key value')) {
-      console.log(error.detail)
+      error = 'Country has already been added, try again'
+    } else if (error.message.toLowerCase().includes('country does not exist')) {
+      error = error.message
     } else {
       console.error(error.name + ': ' + error.message)
       console.log(error)
     }
+
+    return res.render('index.ejs', { error, countries, total: countries.length })
   }
 
   res.redirect("/");
