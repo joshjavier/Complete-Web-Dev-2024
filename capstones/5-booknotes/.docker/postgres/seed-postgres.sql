ea409fa357aecc65c79e41f5c5b1600ec4319829
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-11 17:20:31 +0800

    Update database structure and seed data
    For the `authors` table, we'll use an author's Open Library ID (OLID) as
    the primary key.
    
    I also added a constraint to the `rating` field to limit the values
    between 0 and 5 (inclusive). If I decide to switch to a 10-rating system
    in the future, then I can change this constraint with `ALTER TABLE`.

--- capstones/5-booknotes/.docker/postgres/seed-postgres.sql
+++ capstones/5-booknotes/.docker/postgres/seed-postgres.sql
@@ -13,7 +13,7 @@ CREATE TABLE books (
 );
 
 CREATE TABLE authors (
-  id SERIAL PRIMARY KEY,
+  olid varchar(20) PRIMARY KEY,
   name varchar(100) NOT NULL
 );
 
@@ -23,12 +23,13 @@ CREATE TABLE booknotes (
   content text NOT NULL,
   rating numeric(3,1) NOT NULL,
   date_read timestamp with time zone NOT NULL,
-  PRIMARY KEY (user_id, book_id)
+  PRIMARY KEY (user_id, book_id),
+  CONSTRAINT check_rating_is_valid CHECK (rating >= 0 AND rating <= 5)
 );
 
 CREATE TABLE authorship (
-  book_id integer REFERENCES books,
-  author_id integer REFERENCES authors,
+  book_id integer REFERENCES books ON DELETE CASCADE,
+  author_id varchar(20) REFERENCES authors ON DELETE CASCADE,
   PRIMARY KEY (book_id, author_id)
 );
 
@@ -41,14 +42,14 @@ INSERT INTO books (title, isbn) VALUES
   ('Hannibal Pride of Carthage', '9780553814910'),
   ('One Small Step Can Change Your Life', '9780761180326'),
   ('Zen and the Art of Motorcycle Maintenance', '9780060589462');
-INSERT INTO authors (name) VALUES
-  ('David Anthony Durham'),
-  ('Robert Maurer Ph.D.'),
-  ('Robert M. Pirsig');
+INSERT INTO authors (olid, name) VALUES
+  ('OL1392732A', 'David Anthony Durham'),
+  ('OL7708362A', 'Robert Maurer Ph.D.'),
+  ('OL70357A', 'Robert M. Pirsig');
 INSERT INTO authorship (book_id, author_id) VALUES
-  (1, 1),
-  (2, 2),
-  (3, 3);
+  (1, 'OL1392732A'),
+  (2, 'OL7708362A'),
+  (3, 'OL70357A');
 INSERT INTO booknotes (user_id, book_id, content, rating, date_read) VALUES
   (1, 1, 'Stumbled upon this one in a booksale and was hooked by the engaging writing style.', 4.0, now()),
   (1, 2, 'Great book on kaizen and the concept of continuous improvement', 4.5, now()),
