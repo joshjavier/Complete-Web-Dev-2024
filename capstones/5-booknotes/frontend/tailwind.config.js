d95e71dfba45653a95072918bdbe738bf7e84bb6
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-12 05:00:25 +0800

    Set up frontend tooling with esbuild and tailwind

---
+++ capstones/5-booknotes/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './js/**/*.js',
+    './css/**/*.css',
+    '../backend/**/*.ejs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
