b107f7fcb43f19495286399b96453c93b1a0f39c
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-12 23:19:37 +0800

    Accept query params for sorting booknotes

--- capstones/5-booknotes/backend/routes/index.js
+++ capstones/5-booknotes/backend/routes/index.js
@@ -3,8 +3,19 @@ const router = require('express').Router()
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const booknotes = await findBooknotesByUser(1)
-  console.log(booknotes)
+  let sort = []
+  switch (req.query.sort) {
+    case 'rating':
+      sort = ['booknotes.rating', 'DESC']
+      break
+    case 'recency':
+      sort = ['booknotes.date_read', 'DESC']
+      break
+    case 'title':
+      sort = ['books.title', 'ASC']
+      break
+  }
+  const booknotes = await findBooknotesByUser(1, { sort })
   res.render('index', { title: "Booknotes by Josh", booknotes })
 })
 
--- capstones/5-booknotes/backend/services/booknotes.js
+++ capstones/5-booknotes/backend/services/booknotes.js
@@ -1,14 +1,20 @@
 const db = require('../util/db')
 
-async function findBooknotesByUser(id = 1) {
-  const booknotes = await db.any(`
+async function findBooknotesByUser(id = 1, { sort = [] } = {}) {
+  let orderBy = ''
+  if (sort.length) orderBy = `ORDER BY ${sort[0]} ${sort[1]}`
+
+  const query = `
     SELECT booknotes.user_id, booknotes.book_id, books.title, books.isbn, authors.name AS author, booknotes.content, booknotes.rating, booknotes.date_read
     FROM booknotes
     INNER JOIN books ON books.id = booknotes.book_id
     INNER JOIN authorship ON authorship.book_id = booknotes.book_id
     INNER JOIN authors ON authors.olid = authorship.author_id
-    WHERE booknotes.user_id = $1;
-  `, [id])
+    WHERE booknotes.user_id = $1
+    ${orderBy};
+  `
+
+  const booknotes = await db.any(query, [id])
 
   return booknotes
 }
