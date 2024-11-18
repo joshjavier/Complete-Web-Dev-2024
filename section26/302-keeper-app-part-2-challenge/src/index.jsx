b169a302c0f22526b522252293a9660d3f8f7b8a
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-11-19 05:44:27 +0800

    Done: Keeper App part 2 challenge

--- section26/302-keeper-app-part-2-challenge/src/index.jsx
+++ section26/302-keeper-app-part-2-challenge/src/index.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
 //Challenge. Render all the notes inside notes.js as a seperate Note
 //component.
